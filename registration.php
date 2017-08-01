ebb36a3501f973ed4d81bc827915703a22c51f5b
Bruno Dias <ibroopi@gmail.com>
2017-08-01 11:18:43 +0100

    Fixed wrong module name on registration file

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'OuterEdge_Lifetimesalesfordashboard',
+    'OuterEdge_LifetimeSalesForDashboard',
     __DIR__
 );
